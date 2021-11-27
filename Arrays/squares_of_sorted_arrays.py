896a6575d3c04ca04e535a860d53c9d1f36aa249
Manaswini Ragamouni <Manaswini1920@gmail.com>
2021-11-26 19:58:16 -0500

    day2

--- Arrays/squares_of_sorted_arrays.py
+++ Arrays/squares_of_sorted_arrays.py
@@ -13,4 +13,18 @@ def square_of_sorted_array(nums):
         nums[idx]=nums[idx] **2
     return sorted(nums)
 
+"""
+approach 2:
+final=[None for _ in nums]
+left =0
+right=len(nums)-1
+for i in range(len(nums)-1,-1,-1):
+    if abs(nums[left])>abs(nums[right]):
+        final[i]=nums[left]**2
+        left+=1
+    else:
+        final[i]=nums[right]**2
+        right-=1
+return final
+"""
 print(square_of_sorted_array([-4,-1,0,3,10]))
---
+++ Arrays/third_max.py
@@ -0,0 +1,34 @@
+"""
+Given an integer array nums, return the third distinct maximum number in this array.
+ If the third maximum does not exist, return the maximum number.
+ Input: nums = [3,2,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2.
+The third distinct maximum is 1.
+Example 2:
+
+Input: nums = [1,2]
+Output: 2
+Explanation:
+The first distinct maximum is 2.
+The second distinct maximum is 1.
+The third distinct maximum does not exist, so the maximum (2) is returned instead.
+Input: nums = [2,2,3,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+The third distinct maximum is 1.
+"""
+def third_max(nums):
+    maximums=set()
+    for i in nums:
+        maximums.add(i)
+        if len(maximums)>3:
+            maximums.remove(min(maximums))
+    if len(maximums)==3:
+        return min(maximums)
+    return max(maximums)
+print(third_max([1,2,3,4,5,6]))
