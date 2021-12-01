f20667cb14d9a57f71560588a7a479e2917f7814
Manaswini Ragamouni <Manaswini1920@gmail.com>
2021-12-01 15:51:48 -0500

    day 4

--- Strings/twosum_ii.py
+++ Strings/twosum_ii.py
@@ -4,3 +4,13 @@ Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 <
 Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
 The tests are generated such that there is exactly one solution. You may not use the same element twice.
 """
+def twosumii(nums,target):
+    d=dict()
+    for i,j in enumerate(nums):
+        compliment =target-j
+        if compliment not in d:
+            d[j]=i
+        else:
+            return [i,d[compliment]]
+
+print(twosumii([2,7,11,15], 9)) #index 0,1
