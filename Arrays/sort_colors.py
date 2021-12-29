f54a2497a87be9be9b282c3dd8ba73db15b26f43
Manaswini Ragamouni <Manaswini1920@gmail.com>
2021-12-28 22:47:10 -0500

    sort colors

---
+++ Arrays/sort_colors.py
@@ -0,0 +1,15 @@
+def sort_colors(arr):
+    p0,curr=0,0
+    last=len(arr)-1
+    while curr<=last:
+        if arr[curr]==2:
+            arr[curr],arr[last]=arr[last],arr[curr]
+            last-=1
+        elif arr[curr]==0:
+            arr[p0],arr[curr]=arr[curr],arr[p0]
+            curr+=1
+            p0+=1
+        else:
+            curr+=1
+    return arr
+print(sort_colors([2,0,1,0,1]))
